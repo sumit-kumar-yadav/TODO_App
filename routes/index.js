736507e8307124e72cdf53836ed294bfa40defcd
Sumit <sumityadav0376@gmail.com>
2021-03-09 17:00:19 +0530

    Add comments

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,9 @@ const deleteListController = require('../controllers/delete_list_controller');
 console.log('router loaded');
 
 
-router.get('/', homeController.home);
-router.post('/create-list', addListController.add_list);
-router.post('/delete-list', deleteListController.delete_list);
+router.get('/', homeController.home);        // Simply fetching home page
+router.post('/create-list', addListController.add_list);         // Route to create a list
+router.post('/delete-list', deleteListController.delete_list);    // Route to delete a list
 
 
 module.exports = router;
